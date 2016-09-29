ac5f7d996f4a2f394227e7dfa998becde4b709ca
Juliano Toriani <julianotoriani@gmail.com>
2016-09-29 17:39:43 -0400

    add items listing

--- src/main/java/br/edu/ifam/saf/api/ItensEndpoint.java
+++ src/main/java/br/edu/ifam/saf/api/ItensEndpoint.java
@@ -2,8 +2,10 @@ package br.edu.ifam.saf.api;
 
 
 import br.edu.ifam.saf.api.data.ErrorMessageData;
+import br.edu.ifam.saf.api.data.ItemsData;
 import br.edu.ifam.saf.api.data.LoginData;
 import br.edu.ifam.saf.api.util.Validation;
+import br.edu.ifam.saf.data.Item;
 import br.edu.ifam.saf.exception.ValidacaoError;
 
 import javax.ejb.Stateless;
@@ -11,6 +13,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
 
 @Path("/")
 @Stateless
@@ -27,6 +30,18 @@ public class ItensEndpoint {
         } catch (ValidacaoError ex) {
             return Responses.badRequest(new ErrorMessageData(ex.getMessage()));
         }
+
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Path("/itens")
+    public Response itens() {
+        return Response.ok().entity(new ItemsData(
+                new Item("Jet Ski", 20.0),
+                new Item("Prancha", 5.0))
+        ).build();
+
 
     }
 }
